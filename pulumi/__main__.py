e4c6751d7f24f1d33cc172c1afe9109a9fc767a3
Vitaly Potyarkin <sio.wtf@gmail.com>
2021-02-27 00:15:00 +0300

    Proof-of-concept deployment of a single Hetzner instance

--- pulumi/__main__.py
+++ pulumi/__main__.py
@@ -1,3 +1,8 @@
 """A Python Pulumi program"""
 
 import pulumi
+
+from data import InstanceParams
+from instance import create
+
+create(InstanceParams('test-instance'))
--- pulumi/instance.py
+++ pulumi/instance.py
@@ -5,9 +5,19 @@ Instance definition for a single GitLab runner
 
 from dataclasses import dataclass
 
+import pulumi_hcloud as hcloud
 
 from data import InstanceParams
+from scaling import (
+    require_instances,
+)
 
 
 def create(params: InstanceParams):
     '''Create GitLab runner cloud instance'''
+    server = hcloud.Server(
+        resource_name=params.name,
+        server_type='cx11',
+        image='debian-10'
+    )
+    return server
