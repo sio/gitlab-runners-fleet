71afa590935300e7faea2f05c33cc584fdc171ed
Vitaly Potyarkin <sio.wtf@gmail.com>
2021-02-28 00:45:37 +0300

    Get number of pending CI jobs from GitLabAPI

--- pulumi/gitlab.py
+++ pulumi/gitlab.py
@@ -3,6 +3,8 @@ Communicate with GitLab API
 '''
 
 
+import os
+from itertools import chain
 from cirrus_run import CirrusAPI as GraphqlAPI
 
 
@@ -14,3 +16,44 @@ class GitLabAPI(GraphqlAPI):
 
 def get_pending_jobs() -> int:
     '''Return number of GitLab CI jobs currently waiting for a runner'''
+    token = os.environ['GITLAB_API_TOKEN']
+    api = GitLabAPI(token)
+
+    username_query = '{currentUser {username}}'
+    username = api(username_query)['currentUser']['username']
+
+    jobs_query = '''
+        query GetPendingJobs($namespace: ID!){
+          namespace(fullPath: $namespace) {
+            projects {
+              nodes {
+                waiting: pipelines(status: WAITING_FOR_RESOURCE) {
+                  ...JobDetails
+                }
+                pending: pipelines(status: PENDING) {
+                  ...JobDetails
+                }
+              }
+            }
+          }
+        }
+
+        fragment JobDetails on PipelineConnection {
+          nodes {
+            jobs {
+              nodes {
+                detailedStatus {
+                  text
+                }
+              }
+            }
+          }
+        }
+    '''
+    pending_jobs = 0
+    for project in api(jobs_query, params=dict(namespace=username))['namespace']['projects']['nodes']:
+        for pipeline in chain(project['waiting']['nodes'], project['pending']['nodes']):
+            for job in pipeline['jobs']['nodes']:
+                if job['detailedStatus']['text'] == 'pending':
+                    pending_jobs += 1
+    return pending_jobs
