e46146b9f2af1043de7c0b8e4fd1f072765e7266
Vitaly Potyarkin <sio.wtf@gmail.com>
2021-02-28 00:25:51 +0300

    Reuse API object from cirrus-run project

--- pulumi/gitlab.py
+++ pulumi/gitlab.py
@@ -3,5 +3,14 @@ Communicate with GitLab API
 '''
 
 
+from cirrus_run import CirrusAPI as GraphqlAPI
+
+
+class GitLabAPI(GraphqlAPI):
+    '''Reuse API object from cirrus-run project'''
+    DEFAULT_URL = 'https://gitlab.com/api/graphql'
+    USER_AGENT = 'CI runners fleet manager <https://github.com/sio/gitlab-runners-fleet>'
+
+
 def get_pending_jobs() -> int:
     '''Return number of GitLab CI jobs currently waiting for a runner'''
