6039aaaf37a817c616e3a8724be276d4358ceca5
Vitaly Potyarkin <sio.wtf@gmail.com>
2022-04-18 14:12:21 +0300

    Refactor Pulumi installer
    * Use unified variable names
    * Use newest Pulumi version

--- makefiles/pulumi.mk
+++ makefiles/pulumi.mk
@@ -1,26 +1,27 @@
 # Install Pulumi and required packages on management node (Debian)
 
 
-ifdef PULUMI_AUTO_INSTALL
+ifdef PULUMI_INSTALL_AUTO
 
 ifneq (ok,$(shell $(PULUMI) version >/dev/null 2>&1 && echo ok))
-PULUMI=$(PULUMI_PROJECT)/bin/pulumi
-PULUMI_DOWNLOAD_VERSION?=2.22.0
+PULUMI_INSTALL_VERSION?=3.29.1
+PULUMI_INSTALL_DIR?=./bin
+PULUMI=$(PULUMI_INSTALL_DIR)/pulumi
 endif
 
-check-software: management-node-packages $(PULUMI)
+check-software: $(PULUMI)
 
-$(PULUMI_PROJECT)/bin/pulumi:
+$(PULUMI):
 	mkdir -p "$(dir $@)"
-	curl --fail -sSL "https://get.pulumi.com/releases/sdk/pulumi-v$(PULUMI_DOWNLOAD_VERSION)-linux-x64.tar.gz" \
+	curl --fail -sSL "https://get.pulumi.com/releases/sdk/pulumi-v$(PULUMI_INSTALL_VERSION)-linux-x64.tar.gz" \
 	| tar zxv -C "$(dir $@)" --strip-components=1
 
-.PHONY: management-node-packages
-management-node-packages: $(PULUMI_PROJECT)/bin/os_packages_are_installed
-
-$(PULUMI_PROJECT)/bin/os_packages_are_installed:
+.PHONY: debian-packages
+debian-packages: $(dir $(PULUMI))/.debian-packages-are-installed
+$(dir $(PULUMI))/.debian-packages-are-installed
 	apt-get -y install make python3-venv openssh-client curl
 	mkdir -p $(dir $@)
 	touch $@
 
-endif # PULUMI_AUTO_INSTALL
+
+endif # PULUMI_INSTALL_AUTO
