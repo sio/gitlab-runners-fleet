28c8095f0da3b34ee2f909b3053c07bdf15906ce
Vitaly Potyarkin <sio.wtf@gmail.com>
2022-04-19 12:47:30 +0300

    Create instance configuration from templates

---
+++ src/fleet_manager/provision.py
@@ -0,0 +1,65 @@
+'''
+Working with instance configuration
+'''
+
+import atexit
+from functools import cache
+from pathlib import Path
+from pkg_resources import (
+        cleanup_resources,
+        resource_exists,
+        resource_filename,
+        resource_isdir,
+)
+
+import jinja2
+
+
+def resolve_path(path):
+    '''
+    Resolve path that might be any of the following:
+        - an absolute path to file on filesystem
+        - a relative path to file on filesystem
+        - a relative path to package resource
+    '''
+    if resource_exists(__package__, path):
+        dirname, filename = path.rsplit('/', 1)  # resource paths must not be manipulated via os.path
+        if resource_exists(__package__, dirname) and resource_isdir(__package__, dirname):
+            path = Path(resource_filename(__package__, dirname)) / filename
+        else:
+            path = Path(resource_filename(__package__, path))
+        atexit.register(cleanup_resources)
+    else:
+        path = Path(path)
+    return path
+
+
+def template(path):
+    '''Return Jinja2 template corresponding to provided path'''
+    path = resolve_path(path)
+    if not path.exists:
+        raise ValueError(f'template not found in path: {path}')
+    j2 = jinja_environment(path.parent)
+    return j2.get_template(path.name)
+
+
+@jinja2.pass_environment
+def static_file(j2, name):
+    '''Jinja2 function to render contents of a static file from template directory'''
+    directory = Path(j2.globals['template_directory'])
+    filepath = directory / name
+    with filepath.open() as f:
+        return f.read()
+
+
+@cache
+def jinja_environment(template_directory):
+    '''Create Jinja2 environment for the template directory'''
+    env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_directory))
+    env.filters.update(dict(
+        static_file = static_file,
+    ))
+    env.globals.update(dict(
+        template_directory = str(template_directory),
+    ))
+    return env
