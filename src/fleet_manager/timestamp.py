92fe166c9ae9c1017f64e360b4a2f2372dfadcaa
Vitaly Potyarkin <sio.wtf@gmail.com>
2022-04-20 16:29:04 +0300

    Parse timestamps from arbitrary strings

--- src/fleet_manager/timestamp.py
+++ src/fleet_manager/timestamp.py
@@ -3,7 +3,14 @@ Integer based timestamps
 '''
 
 from datetime import datetime, timezone
+from dateutil.parser import parse
 
 def now():
     '''Integer based current timestamp (Unix epoch)'''
     return int(datetime.now(tz=timezone.utc).timestamp())
+
+
+def from_string(string):
+    '''Read timestamp from string'''
+    dtime = parse(string)
+    return int(dtime.astimezone(timezone.utc).timestamp())
