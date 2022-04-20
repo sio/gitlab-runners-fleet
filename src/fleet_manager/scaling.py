e4515f6d6fbd8f2e7f0d175bbf3a086286c6c110
Vitaly Potyarkin <sio.wtf@gmail.com>
2022-04-20 12:21:22 +0300

    Do not return negative values when calculating max_idle_minutes

--- src/fleet_manager/scaling.py
+++ src/fleet_manager/scaling.py
@@ -14,12 +14,15 @@ class ScalingConfig:
     max_total_instances: int = 10
     max_grow_instances: int = 2
     est_provisioning_minutes: int = 10
-    min_billable_minutes: int = 10
+    min_billable_minutes: int = 60
     max_rerun_delay: int = 10
     max_idle_minutes: Optional[int] = None
 
     def __post_init__(self):
         if self.max_idle_minutes is None or self.max_idle_minutes < 0:
-            self.max_idle_minutes = (self.min_billable_minutes
-                                    -self.max_rerun_delay
-                                    -self.est_provisioning_minutes)
+            self.max_idle_minutes = max(
+                0,
+                self.min_billable_minutes
+                - self.max_rerun_delay
+                - self.est_provisioning_minutes
+            )
