c74d2b32130ae75624c39710b381a181953ca175
Vitaly Potyarkin <sio.wtf@gmail.com>
2022-10-18 11:49:35 +0300

    Allow customization of Pulumi OS/architecture

--- makefiles/pulumi.mk
+++ makefiles/pulumi.mk
@@ -5,6 +5,7 @@ ifdef PULUMI_INSTALL_AUTO
 
 ifneq (ok,$(shell $(PULUMI) version >/dev/null 2>&1 && echo ok))
 PULUMI_INSTALL_VERSION?=3.29.1
+PULUMI_INSTALL_ARCH?=linux-x64
 PULUMI_INSTALL_DIR?=./bin
 PULUMI=$(PULUMI_INSTALL_DIR)/pulumi
 endif
@@ -13,7 +14,7 @@ check-software: $(PULUMI)
 
 $(PULUMI):
 	mkdir -p "$(dir $@)"
-	curl --fail -sSL "https://get.pulumi.com/releases/sdk/pulumi-v$(PULUMI_INSTALL_VERSION)-linux-x64.tar.gz" \
+	curl --fail -sSL "https://get.pulumi.com/releases/sdk/pulumi-v$(PULUMI_INSTALL_VERSION)-$(PULUMI_INSTALL_ARCH).tar.gz" \
 	| tar zxv -C "$(dir $@)" --strip-components=1
 
 .PHONY: debian-packages
