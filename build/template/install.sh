7bcc95725532851a9ede071751904921e8c21140
Vitaly Potyarkin <sio.wtf@gmail.com>
2023-03-27 12:32:10 +0300

    Make base image actually usable

--- build/template/install.sh
+++ build/template/install.sh
@@ -3,38 +3,36 @@ set -euo pipefail
 IFS=$'\n\t'
 set -v
 
+# Root password for interactive debugging
+echo 'root:bydtyn22'|chpasswd # TODO: remove debug password
+
 # Enable cloud-init unconditionally
 cp -v /etc/provision/cloud.cfg.d/* /etc/cloud/cloud.cfg.d
 ln -vsf /lib/systemd/system/cloud-init.target /etc/systemd/system/multi-user.target.wants/
 
-# Root password for interactive debugging
-echo 'root:bydtyn22'|chpasswd # TODO: remove debug password
+# Generate temporary ssh host keys (for Ansible)
+ssh-keygen -A
 
-# Install required packages
+# Install Ansible prerequisites
 apt update
-apt-get install -y \
-    chrony \
+apt-get install -y --no-install-recommends \
     coreutils \
     curl \
-    docker.io \
-    downtimed \
     git \
-    iputils-ping \
     make \
-    nftables \
     python3 \
     python3-apt \
     python3-pip \
     python3-venv \
     sudo \
-    uptimed \
 
+# Launch Ansible. Even though common.yml is a very simple playbook we need to
+# make sure we have a working Ansible install in the resulting image
+make -C /etc/provision common
+
+# Remove temporary host keys
+rm -vf /etc/ssh/*host*key*
+
+# Reduce image size
 apt-get clean
 rm -rf /var/lib/apt/lists/*
-
-# TODO: disable services which are not universally needed
-# - docker
-# - gitlab-runner
-
-# Launch Ansible
-exec make common
