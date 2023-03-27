c431bcaf8195af1e48984e04a6869852cc2b5855
Vitaly Potyarkin <sio.wtf@gmail.com>
2023-03-27 08:36:18 +0000

    Grow initial image before modification

--- build/makefiles/image.mk
+++ build/makefiles/image.mk
@@ -18,6 +18,8 @@ $(INPUT):
 
 $(OUTPUT): $(INPUT)
 	cp $(INPUT) $(OUTPUT)
+	qemu-img resize $(OUTPUT) 5G
+
 
 $(MOUNTPOINT):
 	mkdir -p "$@"
@@ -30,6 +32,9 @@ clean:
 mount: $(OUTPUT) $(MOUNTPOINT)
 	modprobe nbd max_part=8
 	qemu-nbd --connect=$(DEVICE) --format=qcow2 $(OUTPUT)
+	growpart $(DEVICE) 1
+	e2fsck -fy $(DEVICE)p1
+	resize2fs $(DEVICE)p1
 	fdisk -l $(DEVICE)
 	mount $(DEVICE)p1 $(MOUNTPOINT)
 	mount --bind /dev $(MOUNTPOINT)/dev
