479e040ee8548a5f55cccaccaf0ce7f7758cb642
Vitaly Potyarkin <sio.wtf@gmail.com>
2023-03-28 12:06:01 +0300

    Add lifecycle rules for S3 bucket

--- build/bucket.tf
+++ build/bucket.tf
@@ -12,9 +12,27 @@ resource "yandex_storage_bucket" "images" {
   bucket   = var.ycs3_bucket
   max_size = 5 * pow(2, 30) // GB
   anonymous_access_flags {
-    read = true
-    list = false
+    read        = true
+    list        = false
     config_read = false
+  }
+  lifecycle_rule {
+    id      = "Remove outdated OS images"
+    enabled = true
+    prefix  = ""
+    expiration {
+      // Older OS images may contain known security vulnerabilities.
+      // CI pipeline should rotate the image long before this lifecycle rule gets
+      // triggered, but in case it doesn't it's better to fail loudly and
+      // explicitly with a missing image than to continue running an unsafe one
+      days = 6 * 7
+    }
+  }
+  lifecycle_rule {
+    id                                     = "Clean up incomplete uploads"
+    prefix                                 = ""
+    enabled                                = true
+    abort_incomplete_multipart_upload_days = 2
   }
   access_key = yandex_iam_service_account_static_access_key.sa-static-key.access_key
   secret_key = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
