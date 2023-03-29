9b394877cc9055227be2c16805e2dc62e08a754a
Vitaly Potyarkin <sio.wtf@gmail.com>
2023-03-29 17:29:16 +0300

    Make JSON navigator a little more convenient

--- scale/gitlab/json.go
+++ scale/gitlab/json.go
@@ -6,23 +6,45 @@ import (
 
 type jsObject = map[string]any
 
-func jsNested(js jsObject, key ...string) (any, error) {
+// Fetch a value from deeply nested JSON
+func jsNested(js any, key ...string) (any, error) {
+	if len(key) == 0 {
+		return js, nil
+	}
 	var current, next jsObject
+	var ok bool
+	current, ok = js.(jsObject)
+	if !ok {
+		return nil, fmt.Errorf("input does not look like a JS object: %v", js)
+	}
 	var value any
-	var ok bool = true
-	current = js
-	for index, subkey := range key {
-		if !ok {
-			return nil, fmt.Errorf("type conversion failed for key %s (level %d)", key[index-1], index)
-		}
-		value, ok = current[subkey]
+	for index := 0; index < len(key); index++ {
+		value, ok = current[key[index]]
 		if !ok {
 			return nil, fmt.Errorf("key not found: %q (level %d)", key[index], index)
 		}
 		next, ok = value.(jsObject)
-		if ok {
-			current = next
+		if !ok && index < len(key)-1 {
+			return nil, fmt.Errorf("type conversion failed for key %s (level %d): %v", key[index], index, value)
 		}
+		current = next
 	}
 	return value, nil
+}
+
+// Fetch a string from deeply nested JSON
+func jsNestedString(js any, key ...string) (string, error) {
+	var value any
+	var err error
+	value, err = jsNested(js, key...)
+	if err != nil {
+		return "", err
+	}
+	var result string
+	var ok bool
+	result, ok = value.(string)
+	if !ok {
+		return "", fmt.Errorf("key %v is pointing to non-string value: %v", key, value)
+	}
+	return result, nil
 }
