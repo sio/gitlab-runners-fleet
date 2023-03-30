ec9a87a9c2ba2cd6de73dd59b341a5f64b8ddecf
Vitaly Potyarkin <sio.wtf@gmail.com>
2023-03-30 10:50:39 +0300

    Reorder jsGet functions

--- scale/gitlab/json.go
+++ scale/gitlab/json.go
@@ -5,6 +5,29 @@ import (
 	"strings"
 )
 
+// Fetch a string from deeply nested JSON
+func jsGetString(js any, key ...string) (string, error) {
+	return jsGet[string](js, key...)
+}
+
+// Fetch a specific type from deeply nested JSON
+func jsGet[T any](js any, key ...string) (T, error) {
+	var value any
+	var err error
+	value, err = jsGetAny(js, key...)
+	var zero T
+	if err != nil {
+		return zero, err
+	}
+	var result T
+	var ok bool
+	result, ok = value.(T)
+	if !ok {
+		return zero, fmt.Errorf("type error: value for %s is not %T but %T: %v", repr(key), zero, value, value)
+	}
+	return result, nil
+}
+
 // Fetch a value from deeply nested JSON
 func jsGetAny(js any, key ...string) (any, error) {
 	if len(key) == 0 {
@@ -29,29 +52,6 @@ func jsGetAny(js any, key ...string) (any, error) {
 		current = next
 	}
 	return value, nil
-}
-
-// Fetch a string from deeply nested JSON
-func jsGetString(js any, key ...string) (string, error) {
-	return jsGet[string](js, key...)
-}
-
-// Fetch a specific type from deeply nested JSON
-func jsGet[T any](js any, key ...string) (T, error) {
-	var value any
-	var err error
-	value, err = jsGetAny(js, key...)
-	var zero T
-	if err != nil {
-		return zero, err
-	}
-	var result T
-	var ok bool
-	result, ok = value.(T)
-	if !ok {
-		return zero, fmt.Errorf("type error: value for %s is not %T but %T: %v", repr(key), zero, value, value)
-	}
-	return result, nil
 }
 
 // String representation of deep JSON key for error messages
