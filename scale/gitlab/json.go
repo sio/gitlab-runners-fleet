d4fe22dbc7ae492cddce2ac05f5b3fe00f5407db
Vitaly Potyarkin <sio.wtf@gmail.com>
2023-04-03 15:29:16 +0300

    Revert "Remove unused JSON parser functions"
    This reverts commit d41d796813f3bd0b759debcd59fccefb91fdff96.

---
+++ scale/gitlab/json.go
@@ -0,0 +1,61 @@
+package gitlab
+
+import (
+	"fmt"
+	"strings"
+)
+
+// Fetch a string from deeply nested JSON
+func jsGetString(js any, key ...string) (string, error) {
+	return jsGet[string](js, key...)
+}
+
+// Fetch a specific type from deeply nested JSON
+func jsGet[T any](js any, key ...string) (T, error) {
+	var value any
+	var err error
+	value, err = jsGetAny(js, key...)
+	var zero T
+	if err != nil {
+		return zero, err
+	}
+	var result T
+	var ok bool
+	result, ok = value.(T)
+	if !ok {
+		return zero, fmt.Errorf("type error: value for %s is not %T but %T: %v", repr(key), zero, value, value)
+	}
+	return result, nil
+}
+
+// Fetch a value from deeply nested JSON
+func jsGetAny(js any, key ...string) (any, error) {
+	if len(key) == 0 {
+		return js, nil
+	}
+	var current, next map[string]any
+	var ok bool
+	current, ok = js.(map[string]any)
+	if !ok {
+		return nil, fmt.Errorf("input does not look like a JS object: %v", js)
+	}
+	var value any
+	for index := 0; index < len(key); index++ {
+		value, ok = current[key[index]]
+		if !ok {
+			return nil, fmt.Errorf("key not found: %s (level %d)", repr(key[:index+1]), index)
+		}
+		next, ok = value.(map[string]any)
+		if !ok && index < len(key)-1 {
+			return nil, fmt.Errorf("can not go deeper than key %s (level %d): %v", repr(key[:index+1]), index, value)
+		}
+		current = next
+	}
+	return value, nil
+}
+
+// String representation of deep JSON key for error messages
+func repr(key []string) string {
+	const pathSeparator = "."
+	return strings.Join(key, pathSeparator)
+}
---
+++ scale/gitlab/json_test.go
@@ -0,0 +1,103 @@
+package gitlab
+
+import (
+	"testing"
+
+	"bytes"
+	"encoding/json"
+	"strings"
+)
+
+// Check if two inputs produce the same JSON
+func jsonEqual(a, b any) bool {
+	var jsonA, jsonB []byte
+	var errA, errB error
+	jsonA, errA = json.Marshal(a)
+	jsonB, errB = json.Marshal(b)
+	if errA == nil && errB == nil {
+		return bytes.Equal(jsonA, jsonB)
+	}
+	return false
+}
+
+// Tests for jsGetAny()
+func TestNestedAccess(t *testing.T) {
+	input := `{
+		"hello": "world",
+		"foo": {
+			"bar": "baz",
+			"message": {
+				"alice": "bob"
+			}
+		}
+	}`
+	var js map[string]any
+	var err error
+	err = json.Unmarshal([]byte(input), &js)
+	if err != nil {
+		t.Fatal(err)
+	}
+	tests := []struct {
+		path string
+		want any
+		fail bool
+	}{
+		{"hello", "world", false},
+		{"foo.bar", "baz", false},
+		{"foo.message", map[string]string{"alice": "bob"}, false},
+		{"foo.message.alice", "bob", false},
+		{"NONEXISTENT", nil, true},
+		{"hello.NONEXISTENT", nil, true},
+		{"foo.NONEXISTENT", nil, true},
+		{"foo.bar.baz.NONEXISTENT", nil, true},
+		{"foo.message.alice.NONEXISTENT", nil, true},
+	}
+	for index, tt := range tests {
+		var got any
+		got, err = jsGetAny(js, strings.Split(tt.path, ".")...)
+		if !tt.fail && err != nil {
+			t.Errorf("#%d %v: unexpected error: %v", index, tt.path, err)
+			continue
+		}
+		if tt.fail && err == nil {
+			t.Errorf("#%d %v: expected an error, got %v", index, tt.path, got)
+			continue
+		}
+		if got != tt.want && !jsonEqual(got, tt.want) {
+			t.Errorf("#%d %v: got %v, want %v", index, tt.path, got, tt.want)
+			continue
+		}
+	}
+
+	stringTests := []struct {
+		path string
+		want string
+		fail bool
+	}{
+		{"hello", "world", false},
+		{"foo.bar", "baz", false},
+		{"foo.message.alice", "bob", false},
+		{"foo.message", "", true},
+		{"NONEXISTENT", "", true},
+		{"hello.NONEXISTENT", "", true},
+		{"foo.NONEXISTENT", "", true},
+		{"foo.bar.baz.NONEXISTENT", "", true},
+		{"foo.message.alice.NONEXISTENT", "", true},
+	}
+	for index, tt := range stringTests {
+		var got string
+		got, err = jsGetString(js, strings.Split(tt.path, ".")...)
+		if !tt.fail && err != nil {
+			t.Errorf("string#%d %v: unexpected error: %v", index, tt.path, err)
+			continue
+		}
+		if tt.fail && err == nil {
+			t.Errorf("string#%d %v: expected an error, got %v", index, tt.path, got)
+			continue
+		}
+		if got != tt.want {
+			t.Errorf("string#%d %v: got %v, want %v", index, tt.path, got, tt.want)
+			continue
+		}
+	}
+}
