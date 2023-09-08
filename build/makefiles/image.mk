d9dbaa2aeb0b5f92208a3341d77c2fb870fb9f41
Vitaly Potyarkin <sio.wtf@gmail.com>
2023-09-08 08:59:38 +0000

    Make recipes cleaner with .ONESHELL

--- build/makefiles/image.mk
+++ build/makefiles/image.mk
@@ -1,6 +1,9 @@
 #
 # Modify Debian cloud VM image for deploying to Yandex cloud
 #
+SHELL=/bin/bash
+.SHELLFLAGS=-veuo pipefail -c
+.ONESHELL:
 
 REMOTE?=https://cloud.debian.org/images/cloud/bullseye/latest/debian-11-genericcloud-amd64.qcow2
 INPUT?=input.qcow2
@@ -17,12 +20,7 @@ NSPAWN+=--bind-ro="$(TEMPLATE):/tmp/prepare/template"
 NSPAWN+=--bind-ro="$(TEMPLATE)/prepare.service:/etc/systemd/system/prepare.service"
 NSPAWN_RESULT=$(CURDIR)/nspawn.result
 
-LOOP:=$(shell losetup -f)
-LOOPp1=/dev/mapper/$(notdir $(LOOP))p1
-
 INTERNET:=$(shell ip route show default | sed -n 's/.* dev \([^\ ]*\) .*/\1/p')
-TMP_FIREWALL:=$(shell mktemp -t firewall.XXXXXX)
-TMP_FORWARDING:=$(shell mktemp -t forwarding.XXXXXX)
 
 ifeq (yes,$(COMPACT))
 QEMU_CONVERT_ARGS+=-c
@@ -35,20 +33,16 @@ $(INPUT):
 	curl -sSL "$(REMOTE)" -o "$@"
 
 $(SCRATCH): $(INPUT)
-ifeq (,$(LOOP))
-	$(error Required variable not defined: LOOP)
-endif
 	qemu-img convert -f qcow2 -O raw $(INPUT) $(SCRATCH)
 	qemu-img resize -f raw $(SCRATCH) 5G
 	fdisk -l $(SCRATCH)
 	growpart $(SCRATCH) 1
-	losetup $(LOOP) $(SCRATCH)
-	kpartx -av $(LOOP)
-	e2fsck -fy $(LOOPp1)
-	resize2fs $(LOOPp1)
-	sync
-	kpartx -dv $(LOOP)
-	losetup -d $(LOOP)
+	PART=$$(kpartx -av $(SCRATCH) | tee /dev/stderr | awk '$$3 ~ /p1$$/ { print "/dev/mapper/" $$3 }')
+	test -n "$$PART"
+	e2fsck -fy "$$PART"
+	resize2fs "$$PART"
+	sync; sync; sync
+	kpartx -dv $(SCRATCH)
 	fdisk -l $(SCRATCH)
 
 .PHONY: prepare
@@ -57,8 +51,10 @@ $(SCRATCH).done: $(SCRATCH)
 ifeq (,$(INTERNET))
 	$(error Required variable not defined: INTERNET)
 endif
-	sysctl -n net.ipv4.ip_forward > $(TMP_FORWARDING)
-	iptables-save -f $(TMP_FIREWALL)
+	TMP_FIREWALL=$$(mktemp -t firewall.XXXXXX)
+	TMP_FORWARDING=$$(mktemp -t forwarding.XXXXXX)
+	sysctl -n net.ipv4.ip_forward > "$$TMP_FORWARDING"
+	iptables-save -f "$$TMP_FIREWALL"
 	systemctl is-active systemd-networkd || systemctl start systemd-networkd
 	networkctl
 	ip addr
@@ -67,9 +63,9 @@ endif
 	iptables -A FORWARD -i ve-+ -o $(INTERNET) -j ACCEPT
 	touch "$(NSPAWN_RESULT)"
 	$(NSPAWN) /usr/bin/systemd --unit=prepare.service
-	iptables-restore $(TMP_FIREWALL)
-	sysctl net.ipv4.ip_forward=$$(cat $(TMP_FORWARDING))
-	$(RM) -v $(TMP_FIREWALL) $(TMP_FORWARDING)
+	iptables-restore "$$TMP_FIREWALL"
+	sysctl net.ipv4.ip_forward=$$(cat "$$TMP_FORWARDING")
+	$(RM) -v "$$TMP_FIREWALL" "$$TMP_FORWARDING"
 	RESULT=$$(cat "$(NSPAWN_RESULT)"); echo "$$RESULT"; [ "$$RESULT" = "success exited 0" ]
 	$(RM) -v "$(NSPAWN_RESULT)"
 	mv -v $(SCRATCH) $@
